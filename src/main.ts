48b24f57326c7bc3d9c63342c4a7c9f041b0d17d
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-09-21 17:59:31 +0300

    Init PostgreSQL

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 5500;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS CRUD')
+    .setDescription('Documentation for REST API')
+    .setVersion('1.0.0')
+    .addTag('Mikita Radzkou')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
+  await app.listen(PORT, () => {
+    return console.log(`Server is running on PORT = ${PORT}`);
+  });
 }
 bootstrap();
